6d9f27af6a53808b7dc7eead4bc8cb2cf141a6a0
Akram <119796144+akrammihyar@users.noreply.github.com>
2023-01-15 13:40:03 +0100

    Update gpt_app.py

--- gpt_app.py
+++ gpt_app.py
@@ -1,6 +1,6 @@
 import Main
 import streamlit as st
 
-st.set_page_config(page_title="Project Date Night", page_icon=":smile:", layout="wide")
+st.set_page_config(page_title="Project 'Date Night'", page_icon=":smile:", layout="wide")
 
 Main.app()
