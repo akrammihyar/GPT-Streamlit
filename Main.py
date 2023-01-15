19c3754c8509f150175aa38b91ff5a48b83e460c
Akram <119796144+akrammihyar@users.noreply.github.com>
2023-01-15 14:13:29 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,20 +1,14 @@
 import streamlit as st
-from model import GeneralModel
-import datetime
+from pandas as pd
+import random
 
 
 def app():
-
-    # Creating an object of prediction service
-    pred = GeneralModel()
-
+    df = pd.read_csv("questions.csv")
     api_key = st.sidebar.text_input("APIkey", type="password")
+
     # Using the streamlit cache
-    @st.cache(allow_output_mutation=True)
-    def process_prompt(input):
-
-        return pred.model_prediction(input=input.strip() + ' ' + str(datetime.datetime.now()) , api_key=api_key)
-
+    @st.cache
     if api_key:
 
         # Setting up the Title
@@ -31,14 +25,10 @@ def app():
         )
 
         if st.button("Generate"):
-            with st.spinner(text="In progress"):
-                report_text = process_prompt(input)
-                st.markdown(report_text)
-                
-        if st.button("Next Card"):
-            with st.spinner(text="In progress"):
-                report_text = process_prompt(input)
-                st.markdown(report_text)
+            question = df.sample(1)["question"].values[0]
+            st.write("---")
+            st.write(question)
+            st.write("---")
                 
     else:
         st.error("🔑 Please enter API Key")
